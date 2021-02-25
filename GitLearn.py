9e350b0df0b147b09049d7921f66043705d887fa
Kumara Venkatesh/LGSIA MPS-1(kumara.venkatesh@lgepartner.com) <kumara.venkatesh@lge.com>
2021-02-25 07:32:16 +0530

    Git fourum print message

--- GitLearn.py
+++ GitLearn.py
@@ -2,5 +2,10 @@ print("Git Learning")
 interested = input("would you like to learn GitHub")
 if interested=='yes':
     print('Welcome to Git Tutorial')
+    print('Would ypu like to join the git forum')
+    
 else:
     print("Your Application Exited")
+    print('Thank you')
+
+
